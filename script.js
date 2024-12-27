3a187c9586ff7a650acdaca53b44295d15e69a34
Digamber Chandrawanshi <98806966+Digamber03@users.noreply.github.com>
2024-12-27 15:17:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//Below for chart preparation
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Python', 'SQL', 'AWS', 'Tableau'],
+//         datasets: [{
+//             label: 'My Skill Level',
+//             data: [95, 85, 70, 80],
+//             backgroundColor: ['red', 'blue', 'green', 'purple'],
+//             borderColor: ['red', 'blue', 'green', 'purple'],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
